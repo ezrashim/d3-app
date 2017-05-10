d9747dea4d1036d657404312b102075a2f522e1f
Ezra Shim <ezrashim@gmail.com>
2017-05-10 18:34:39 -0400

    d3

---
+++ app/controllers/d3_controller.rb
@@ -0,0 +1,15 @@
+class D3Controller < ApplicationController
+
+  def index
+
+  end
+
+  def data
+    respond_to do |format|
+      format.json {
+        render :json => [1,2,3,4,5]
+      }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'd3#index'
+  get 'd3/data', :defaults => { :format => 'json'}
+  resources :d3, only: [:index]
+
 end
